b6ad721d39505a8a372afee8c4e6eb4d5528f340
doofmars <jan-preuss@gmx.de>
2015-10-18 01:49:41 +0200

    Created first basic layout and some logic for adding letters to the alphabet

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,31 @@
  * # MainCtrl
  * Controller of the markovApp
  */
-angular.module('markovApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+ angular.module('markovApp')
+ .controller('MainCtrl', function ($scope) {
+    $scope.data = {alphabet: ['1', '2'], rules:{}, mode: ''};
+    $scope.letter = '';
+    
+    $scope.addLetter = function(keyevent) {
+        //Only accept return key events
+        if (keyevent !== undefined) {
+            if (keyevent.which !== 13) {
+                return;
+            }
+        }
+        //Disallow empty or single space words
+        if ($scope.letter === '' || $scope.letter === ' ') {
+            $scope.letter = '';
+            return;
+        }
+        //Test if letter is already in alphabet
+        if ($.inArray($scope.letter, $scope.data.alphabet) < 0) {
+            $scope.data.alphabet.push($scope.letter);
+            console.log('Added ' + $scope.letter + ' to the alphabet');
+        } else {
+            console.log('Letter already in alphabet');
+        }
+        $scope.letter = '';
+        console.log($scope.data);
+    };
+ });
