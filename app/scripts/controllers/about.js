6408d39262c3cbed4d931955953de0c20861d3db
doofmars <jan-preuss@gmx.de>
2015-10-17 23:46:39 +0200

    began rework using yeoman, angularjs, bower, grunt and bootstrap for development

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name markovApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the markovApp
+ */
+angular.module('markovApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name markovApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the markovApp
+ */
+angular.module('markovApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
