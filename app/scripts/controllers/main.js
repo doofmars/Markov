8ce64fec0daf14bf9d408874d93d27d129a5ea67
doofmars <jan-preuss@gmx.de>
2015-10-31 23:39:24 +0100

    Added markov mode
    Displays Current iteration, highlight change, rule used and reason of termination

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,14 +12,15 @@
     $scope.data = 
     {alphabet: ['1', '2', '3', '4'], 
         rules:[
-            {from:'1b',  terminating:false,  to:''}, 
-            {from:'2b',  terminating:true,  to:''}, 
-            {from:'3b',  terminating:false,  to:''}, 
-            {from:'4a',  terminating:false,  to:'b'}
-            ], mode: 'markov', maxIterations: 100};
+            {from:'3',  terminating:false,  to:'4'},
+            {from:'2',  terminating:true,  to:'3'}, 
+            {from:'1',  terminating:false,  to:'2'} 
+        ], mode: 'markov', maxIterations: 100,
+    input: '123'};
     $scope.letter = '';
     $scope.ruleFrom = '';
     $scope.ruleTo = '';
+    $scope.results = [];
     
     $scope.addLetter = function(keyevent) {
         //Only accept return key events
@@ -67,4 +68,85 @@
         $scope.ruleTo = '';
         $('#ruleFrom').focus();
     };
+    
+    $scope.runAutomat = function(keyevent) {
+        //Only accept return key events
+        if (keyevent !== undefined) {
+            if (keyevent.which !== 13) {
+                return;
+            }
+        }
+        //Disallow empty input
+        if ($scope.data.input === '') {
+            return;
+        }
+        if ($scope.data.mode === 'markov') {
+            var terminatedByRule = runMarkov();
+            
+            if (terminatedByRule > 0) {
+                $scope.results.push({display: 'End1: Terminating rule', used: terminatedByRule});
+            } else if (terminatedByRule < 0){
+                $scope.results.push({display: 'End2: No rule executable', used: terminatedByRule});
+            } else {
+                $scope.results.push({display: 'End3: Max iterations reached', used: terminatedByRule});
+            }
+        } else if ($scope.data.mode === 'semithue') {
+            runSemiThue();
+        }
+        
+    };
+    
+    function runMarkov() {
+        $scope.results = [];
+        $scope.results.push({display: $scope.data.input, used: '-'});
+        var maxIt = $scope.data.maxIterations;
+        var current = $scope.data.input;
+        
+        for (var i = 0; i < maxIt; i++) {
+            var ruleExecutedFlag = false;
+            
+            for (var j = 0; j < $scope.data.rules.length; j++) {
+                var fromRule = $scope.data.rules[j].from;
+                var toRule = $scope.data.rules[j].to;
+                var terminatingRule = $scope.data.rules[j].terminating;
+                
+                var indices = getIndicesOf(fromRule, current);
+                if (indices.length > 0) {
+                    var partA = current.substr(0, indices[0]);
+                    var partB = current.substr(indices[0] + fromRule.length);
+                    
+                    current = partA + toRule + partB;
+                    $scope.results.push({display: partA + '<b>' +  toRule + '</b>' + partB, used: j + 1});
+                    if (terminatingRule) {
+                        return i + 1;
+                    }
+                    ruleExecutedFlag = true;
+                    break;        
+                }
+            }
+            
+            if (!ruleExecutedFlag) {
+                break;
+            }
+        }
+        console.log(i + ' ## ' + maxIt);
+        return i - maxIt;
+    }
+    
+    function runSemiThue() {
+        
+    }
+    
+    //function to find occurrences of an substring within a string, returns an array with position 
+    function getIndicesOf(searchStr, str) {
+        var startIndex = 0;
+        var index;
+        var indices = [];
+
+        while ((index = str.indexOf(searchStr, startIndex)) > -1) {
+            indices.push(index);
+            startIndex = index + 1;
+        }
+        return indices;
+    }
 });
